7a166fb2b7ebd972553f988ebc1dceb444651657
Jerry Cheung <huahuazfh@qq.com>
2019-10-30 19:24:41 +0800

    feat(project): initial commit

---
+++ build/rollup.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const buble = require('rollup-plugin-buble'); 
+const typescript = require('rollup-plugin-typescript');
+const resolve = require('rollup-plugin-node-resolve');
+const tslint = require('rollup-plugin-tslint');
+
+const resolveFile = function(filePath) {
+  return path.join(__dirname, '..', filePath)
+}
+
+module.exports = [
+  {
+    input: resolveFile('src/index.ts'),
+    output: {
+      file: resolveFile('dist/index.js'),
+      format: 'umd',
+      name: 'demo',
+    }, 
+    plugins: [
+      tslint({
+        configuration: resolveFile('/tslint.json'),
+        throwOnError: true
+      }),
+      typescript(),
+      buble(),
+      resolve()
+    ],
+  },
+]
